83bdfc4fa041f2d868c161f2043cbd4b5b3d0109
Bhooma <manjunath.bhooma@accenture.com>
2025-01-28 10:03:00 +0530

    modified variable.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The AWS region to deploy resources"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
